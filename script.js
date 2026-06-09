0a291bdb2e4e1c1a9f53a25f493c2aabb1abab47
fastmovies2026-maker <fastmovies.2026@gmail.com>
2026-06-09 09:11:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const peliculas = {
     anio: "1984",
     genero: "Ciencia ficción",
     sinopsis: "Los Ángeles, año 2029. Las máquinas dominan el mundo. Los rebeldes que luchan contra ellas tienen como líder a John Connor, un hombre que nació en los años ochenta. Para acabar con la rebelión, las máquinas deciden enviar al pasado a un robot -Terminator- cuya misión será eliminar a Sarah Connor, la madre de John, e impedir así su nacimiento",
-    poster: "img/",
-    drive: "TU_ID_DE_GOOGLE_DRIVE"
+    poster: "img/img/terminator-l_cover.jpg",
+    drive: "1MscfZqRAVWdEYEnRFyqGx7v6ym2PS-WV"
   }
 };
 
